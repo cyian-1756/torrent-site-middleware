1b973591253034bb03974782d1e751c2ec1a3a30
cyian-1756 <devnull64@vfemail.net>
2018-08-11 00:30:20 -0400

    Added abilty to turn sites into rss feeds. Added support for urning 1229x.to into a rss feed

--- middleware.go
+++ middleware.go
@@ -6,15 +6,23 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/feeds"
 )
 
 const (
 	debug = true
 	dURL  = "https://www.demonoid.pw/genlb.php?genid="
+	leetxURL = "https://1337x.to"
 )
+
+func magnetToTorrent(magnetLink string) []byte {
+	return []byte("d10:magnet-uri" + strconv.Itoa(len(magnetLink)) + ":" + magnetLink + "e")
+}
 
 func getPage(url string) []byte {
 	resp, err := http.Get(url)
@@ -77,6 +85,9 @@ func extract(rw http.ResponseWriter, req *http.Request) {
 	case "demonoid":
 		debugPrint("Running " + params["site"] + " extractor")
 		extractDemonoid(params["url_id"], rw)
+	case "1337x":
+		debugPrint("Running " + params["site"] + " extractor")
+		extract1337x(params["url_id"], rw)
 	default:
 		debugPrint("No extractor for " + params["site"])
 
@@ -96,11 +107,89 @@ func extractDemonoid(id string, rw http.ResponseWriter) {
 	})
 }
 
+func extract1337x(id string, rw http.ResponseWriter) {
+	debugPrint("Starting")
+	doc := getDoc("https://1337x.to/torrent/" + id + "//")
+	debugPrint("Finding torrent links")
+	sel := doc.Find("a")
+	debugPrint("got torrent links")
+	for i := range sel.Nodes {
+		debugPrint("Looping over torrent links")
+		s := sel.Eq(i)
+		e, _ := s.Attr("href")
+		if !strings.Contains(e, "magnet:") && strings.Contains(e, "/torrent/") {
+			debugPrint("Downloading page " + e)
+			rw.Write([]byte(getPage(e)))
+			return
+
+		}
+	}
+		
+}
+
+func generateFeed(title string, link string, des string) *feeds.Feed {
+	feed := &feeds.Feed{
+        Title:       title,
+        Link:        &feeds.Link{Href: link},
+        Description: des,
+        Author:      &feeds.Author{Name: "test"},
+        Created:     time.Now(),
+	}
+	return feed
+}
+
+func generateItem(title string, link string) *feeds.Item {
+	return &feeds.Item{
+		Title:       title,
+		Link:        &feeds.Link{Href: link},
+		Description: "",
+		Author:      &feeds.Author{Name: "", Email: ""},
+		Created:     time.Now(),
+	}
+}
+
+func scrape1337x(feed *feeds.Feed, urlToScrape string) string {
+	doc := getDoc(urlToScrape)
+	doc.Find("tbody > tr > td > a").Each(func(i int, s *goquery.Selection) {
+		e, _ := s.Attr("href")
+		debugPrint(e)
+		if strings.Contains(e, "/torrent/") {
+			feed.Add(generateItem(s.Text(), "http://127.0.0.1:5000/extractor/1337x/" + strings.Split(e, "/")[2]))
+		}
+
+	})
+	e, err := feed.ToRss()
+	handleError(err)
+	return e
+
+}
+
+func siteToRss(rw http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	site := params["site"]
+	url := params["url"]
+	debugPrint("Using siteToRss for " + site)
+	debugPrint("Turning " + url + " into a rss feed")
+	feed := generateFeed(url + " rss feed", "127.0.0.1/rss/" + site + "/" + url, "A rss feed generated from " + url)
+
+
+	switch site {
+	case "1337x":
+		fmt.Fprintf(rw, scrape1337x(feed, url))
+	default:
+		rw.WriteHeader(400)
+		fmt.Fprintf(rw, "No rss generator for that site")
+	}
+	
+
+}
+
 func main() {
 	log.Println("Setting up routes")
 	router := mux.NewRouter()
 	router.HandleFunc("/", proxyRequest).Queries("url", "{url}", "use_regex", "{use_regex}", "to_replace", "{to_replace}", "replacement", "{replacement}").Methods("GET")
 	router.HandleFunc("/extractor/{site}/{url_id}", extract).Methods("GET")
+	router.HandleFunc("/rss/{site}", siteToRss).Queries("url", "{url}").Methods("GET")
 	log.Println("Routes set")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
